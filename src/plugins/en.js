a25df1ca203ed2f453b23cc3f079842b0dc24e41
stayacid <stayacid@gmail.com>
2021-07-07 21:56:27 +0300

    add deploy fix i18n

--- src/plugins/en.js
+++ src/plugins/en.js
@@ -1,8 +1,7 @@
+import en from 'vuetify/lib/locale/en'
+
 export default {
+  ...en,
   search: "Search",
   address: "address",
-
-  namespace: {
-    key3: "key 3 internationalization"
-  }
 }
--- src/plugins/ru.js
+++ src/plugins/ru.js
@@ -1,8 +1,7 @@
+import ru from 'vuetify/lib/locale/ru'
+
 export default {
+  ...ru,
   search: "Искать",
   address: "адрес",
-
-  namespace: {
-    key3: "key 3 internationalization"
-  }
 }
